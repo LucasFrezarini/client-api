22781b49f66a7a91b3bd73ce90afd0e26b0dcf86
Lucas Frezarini <lucas.frezarini@hypnobox.com.br>
2018-07-30 09:12:43 -0300

    Finishing schemas unit testing

--- src/tests/unit/config/mongoose/schemas/ContactsSchemaTest.ts
+++ src/tests/unit/config/mongoose/schemas/ContactsSchemaTest.ts
@@ -14,4 +14,56 @@ describe("Contacts Schema Unit Test", () => {
       done();
     });
   });
+
+  it("Should throw error if lastName is empty", (done) => {
+    const Schema = mongoose.model("contact", ContactSchema);
+    const contact = new Schema();
+
+    contact.validate((err) => {
+      // tslint:disable-next-line:no-unused-expression
+      expect(err.errors.lastName).to.exist;
+      done();
+    });
+  });
+
+  it("Should throw error if email is empty", (done) => {
+    const Schema = mongoose.model("contact", ContactSchema);
+    const contact = new Schema();
+
+    contact.validate((err) => {
+      // tslint:disable-next-line:no-unused-expression
+      expect(err.errors.email).to.exist;
+      done();
+    });
+  });
+
+  it("Deleted value should be false by default", () => {
+    const Schema = mongoose.model("contact", ContactSchema);
+    const contact = new Schema() as any;
+
+    // tslint:disable-next-line:no-unused-expression
+    expect(contact.deleted).to.be.false;
+  });
+
+  it("Should require phone number if a phone item is passed to the phones array", (done) => {
+    const Schema = mongoose.model("contact", ContactSchema);
+    const contact = new Schema() as any;
+
+    contact.phones.push({});
+
+    contact.validate((err) => {
+      // tslint:disable-next-line:no-unused-expression
+      expect(err.errors["phones.0.number"]).to.exist;
+      done();
+    });
+  });
+
+  it("Sort of phone must be 'residential' by default", () => {
+    const Schema = mongoose.model("contact", ContactSchema);
+    const contact = new Schema() as any;
+
+    contact.phones.push({});
+
+    expect(contact.phones[0].sort).to.be.equals("residential");
+  });
 });
